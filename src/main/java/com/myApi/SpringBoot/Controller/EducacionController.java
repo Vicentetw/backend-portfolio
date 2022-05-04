3bcf2478d205ce6051c51b129f7bc8e3d8365847
Vicentetw <perrottavicente@gmail.com>
2022-05-04 07:54:44 -0300

    modifico cors a *

--- src/main/java/com/myApi/SpringBoot/Controller/EducacionController.java
+++ src/main/java/com/myApi/SpringBoot/Controller/EducacionController.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@CrossOrigin(origins = "https://portfolio-angular-81057.web.app/")
-
+@RequestMapping("/education")
+/*@CrossOrigin(origins = "https://portfolio-angular-81057.web.app/")*/
+@CrossOrigin(origins = "*")
 public class EducacionController {
     
 @Autowired
